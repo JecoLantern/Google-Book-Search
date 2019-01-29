ba8a8360f2ef605ee0baf970cef3ce2d7defb535
JecoLantern <jbayonke@hotmail.com>
2019-01-28 23:23:49 -0800

    fixing

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/public"));
+    app.use(express.static("front-end/public"));
 }
 
 //routes
